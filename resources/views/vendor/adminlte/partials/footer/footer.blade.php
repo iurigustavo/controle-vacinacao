4cb307502fb0d6610c4eda2a52d5dce2b40287f5
Iuri Gustavo <gustavo.iurigmail.com>
2021-05-07 20:53:15 -0400

    style: correção footer

--- resources/views/vendor/adminlte/partials/footer/footer.blade.php
+++ resources/views/vendor/adminlte/partials/footer/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Versão</b> {{env('APP_VERSION')}}
     </div>
-    <strong>© {{now()->year}} - Controle de Vacinação - Desenvolvido por
+    <strong>{{now()->year}} - Controle de Vacinação - Desenvolvido por
         <a href="https://github.com/iurigustavo" target="_blank">Iúri Gustavo</a> -
         <a href="https://smti.portovelho.ro.gov.br/" target="_blank">DESIS/SMTI - Porto Velho/RO</a>
     </strong>
